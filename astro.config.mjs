10f9474f2a1c456cd17f721f40b1361ca690f49f
kdbush64-create <kdbush64@gmail.com>
2026-04-17 09:21:30 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import keystatic from '@keystatic/astro';
+import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({ imageService: 'compile' }),
-  integrations: [keystatic()],
+  integrations: [react(), keystatic()],
   site: 'https://v64otd.com',
 });
