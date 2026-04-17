e2d65929c2181d5832091ee3e8a41776bf6ba65f
kdbush64-create <kdbush64@gmail.com>
2026-04-17 10:39:58 -0500

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 
 const postSchema = z.object({
   title: z.string(),
-  date: z.string(),
+  date: z.coerce.string(),
   description: z.string().optional(),
   tags: z.array(z.string()).optional(),
   affiliate: z.boolean().optional(),
