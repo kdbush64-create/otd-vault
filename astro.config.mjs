0c71bbc35577b9219ad955045275be4c2737acfc
kdbush64-create <kdbush64@gmail.com>
2026-04-19 17:33:47 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import cloudflare from '@astrojs/cloudflare';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({ imageService: 'compile' }),
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), sitemap()],
   site: 'https://v64otd.com',
 });
